ebed4565f7ac712036459bc263c0a68bb8613894
Braydon Jones <bradofrado@gmail.com>
2024-03-20 11:38:54 -0600

    Add developer video

--- src/components/core/video.tsx
+++ src/components/core/video.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 
-export function Video() {
+export const Video: React.FunctionComponent<{src: string}> = ({src}) => {
   const [hasWindow, setHasWindow] = useState(false);
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -12,7 +12,7 @@ export function Video() {
   
     return (
       hasWindow ? <div style={{position: "relative",paddingBottom: "53.73134328358209%",height: 0}}>
-        <iframe src="https://www.loom.com/embed/c569217786984bebb99c72aa69dfb2fd?sid=62de6885-0bd8-4532-9cd1-17bf634e0649" frameBorder="0" allowFullScreen style={{position: "absolute",top: 0,left: 0,width: "100%",height: "100%"}}></iframe>
+        <iframe src={src} frameBorder="0" allowFullScreen style={{position: "absolute",top: 0,left: 0,width: "100%",height: "100%"}}></iframe>
         </div> : null
     )
   }
--- src/components/designer-developer-switcher.tsx
+++ src/components/designer-developer-switcher.tsx
@@ -1,19 +1,44 @@
 'use client';
+import { useState } from "react";
 import { Header } from "./core/header"
 import { ToggleButton } from "./core/toggle-button"
 import { Video } from "./core/video"
+import { useSearchParams } from "next/navigation";
+
+interface RoleContent {
+    name: string;
+    src: string;
+    linkText: string;
+    link: 'developer' | 'designer'
+}
+
+const content: Record<'developer' | 'designer', RoleContent> = {
+    designer: {
+        name: 'Pure Harmony for Designers.',
+        src: 'https://www.loom.com/embed/c569217786984bebb99c72aa69dfb2fd?sid=62de6885-0bd8-4532-9cd1-17bf634e0649',
+        linkText: 'I am a developer.',
+        link: 'developer'
+    },
+    developer: {
+        name: 'Pure Harmony for Developers.',
+        src: 'https://www.loom.com/embed/ebbf59d3793d41f2a9216ec411ff7a99?sid=5a96b421-9959-49a9-86d8-6ac176dad977',
+        linkText: 'I am a product designer.',
+        link: 'designer'
+    }
+}
 
 export const DesignerDeveloperSwitcher: React.FunctionComponent = () => {
+    const [role, setRole] = useState<'designer' | 'developer'>('designer');
+    const data = content[role];
+
     return (
         <div className="container-3">
-            {/* <div>
-                <ToggleButton buttons={[{id: '1', label: 'Hello'}]} selected="1" setSelected={() => undefined}></ToggleButton>
-            </div> */}
-            <div className="header-wrapper mb-8">
-                <Header>Pure Harmony for Designers and Developers.</Header>
+            <div className="header-wrapper mb-10">
+                <Header className="!text-[50px]" level={1}>{data.name}</Header>
+                <button className="text-base md:text-xl text-[#88939d] hover:text-[#11283b]" onClick={() => setRole(data.link)}>{data.linkText}</button>
             </div>
             <div className="mb-5">
-                <Video/>
+                <Video key={data.name} src={data.src}/>
             </div>
       </div>
     )
