6efba7807a0d546b54414c20acc39686a0a6ea74
Braydon Jones <bradofrado@gmail.com>
2024-04-19 00:18:03 +0000

    Update files content

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
       <div className="container-3">
         <div className="hero-wrapper-2">
           <div className="hero-split-2">
-            <Header className="text-3xl" level={3}>Fully integrate with an existing codebase.</Header>
+            <Header className="text-3xl pb-2.5" level={3}>Fully integrate with an existing codebase.</Header>
             <p className="margin-bottom-24px-3">Harmony constructs a “map” of your UI code, allowing your team to edit your app with modern design tools.</p>
           </div>
           <div className="hero-split-2">
@@ -65,7 +65,7 @@ export default function Home() {
       <div className="container-3">
         <div className="hero-wrapper-2">
           <div className="hero-split-2">
-            <Header className="text-3xl" level={3}>Eliminate your developer’s design burden.</Header>
+            <Header className="text-3xl pb-2.5" level={3}>Eliminate your developer’s design burden.</Header>
             <p className="margin-bottom-24px-3">Ship design changes instantly with production-ready code, saving weeks of developer time. Changes made directly in your codebase with seamless Github integration.</p>
           </div>
             <img src="/images/Harmony.ai - 2024-02-01T125854.129 1 (1).svg" loading="lazy" width="293" height="Auto" alt="" className="shadow-two-2"/>
@@ -73,8 +73,8 @@ export default function Home() {
       </div>
       <div className="container-3">
         <div className="hero-wrapper-2">
-          <div className="hero-split-2">
-            <Header className="text-3xl" level={3}>Accelerate with instant design iteration.</Header>
+          <div className="hero-split-2 items-start justify-center">
+            <Header className="text-3xl pb-2.5" level={3}>Accelerate with instant design iteration.</Header>
             <p className="margin-bottom-24px-3">See your design changes instantly in the application without having to wait for your developer. Prototype new features and designs in minutes.</p>
           </div>
           <div className="hero-split-2">
@@ -84,8 +84,8 @@ export default function Home() {
       </div>
       <div className="container-3">
         <div className="hero-wrapper-2">
-          <div className="hero-split-2">
-            <Header className="text-3xl" level={3}>Developer tools built for designers.</Header>
+          <div className="hero-split-2 items-start">
+            <Header className="text-3xl pb-2.5" level={3}>Developer tools built for designers.</Header>
             <p className="margin-bottom-24px-3">Build inside your application with a familiar design canvas. Create with your team’s design elements, components, and br/and. Absolutely no technical context required.</p>
           </div>
           <img src="/images/Harmony.ai - 2024-02-01T125708.592 1 (1).svg" loading="lazy" width="288" alt="" className="shadow-two-2"/>
@@ -95,16 +95,16 @@ export default function Home() {
     <section className="hero-heading-left">
       <div className="container-3">
         <div className="header-wrapper mb-6">
-          <Header level={2}>How does <Emphasize>Harmony</Emphasize> work?</Header>
+          <Header className="mr-0 pr-0" level={2}>How does <Emphasize>harmony</Emphasize> work?</Header>
         </div>
         <div className="mb-5">
             <Video src="https://www.loom.com/embed/3e6c689d979b4251903e7a5c67312bfc?sid=5489f8cb-87e9-46a3-9af6-9039df000d47"/>
         </div>
       </div>
     </section>
-    <section className="hero-without-image-2">
-      <div className="hero-wrapper-two-2">
-        <Header className="text-3xl" level={3}>Join the beta today.</Header>
+    <section className="hero-without-image-2 pt-20">
+      <div className="hero-wrapper-two-2 justify-start py-5">
+        <Header className="text-3xl mb-2.5" level={3}>Join the beta today.</Header>
         <a href="https://j48inpgngmc.typeform.com/to/Ch60XpCt" className="button w-button">Join the Beta</a>
       </div>
     </section>
